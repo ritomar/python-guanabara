d727b3b228d991db497550fcf0162284d0d7e568
ritomar <ritomar@hotmail.com>
2019-02-23 22:05:00 -0300

    Exercício Python #005 - Antecessor e Sucessor

---
+++ ex005.py
@@ -0,0 +1,9 @@
+"""
+    Exercício Python #005 - Antecessor e Sucessor
+
+    Faça um progrma que leia um número inteiro e mostre na tela o seu
+    sucessor e o seu antecessor.
+"""
+
+n = int(input("Digite um número: "))
+print(f'Analisando o valor {n}, seu antecessor é {n - 1} e o sucessor é {n + 1}.')
