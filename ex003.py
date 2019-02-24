67171a4ba520db189237ff810378902dff199f8b
ritomar <ritomar@hotmail.com>
2019-02-23 21:40:36 -0300

    Exercício Python #003 - Somando dois número

---
+++ ex003.py
@@ -0,0 +1,11 @@
+"""
+    Exercício Python #003 - Somando dois número
+
+    Faça um programa que leia o ome de uma pessoa e mostre uma
+    mensagem de boas-vindas.
+"""
+
+a = int(input("Digite um valor: "))
+b = int(input("Digite outro valor: "))
+soma = a + b
+print(f'A soma entre {a} e {b} vale {soma}.')
