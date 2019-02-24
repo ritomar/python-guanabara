0020bb3f673d2cb9835f89f40b346bd5fb969658
ritomar <ritomar@hotmail.com>
2019-02-23 21:36:53 -0300

    Exercício Python #002 - Respondendo ao usuário

---
+++ ex002.py
@@ -0,0 +1,9 @@
+"""
+    Exercício Python #002 - Respondendo ao usuário
+
+    Faça um programa que leia o ome de uma pessoa e mostre uma
+    mensagem de boas-vindas.
+"""
+
+nome = input("Digite seu nome: ")
+print(f'Olá, {nome}!')
