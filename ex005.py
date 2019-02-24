eb3fe7e0b5ce9db87b48a24f897ad59fefaa06fe
ritomar <ritomar@hotmail.com>
2019-02-23 22:16:22 -0300

    Exercício Python #006 - Dobro, Triplo e Raiz Quadrada

--- ex005.py
+++ ex005.py
@@ -8,4 +8,4 @@
 n = int(input("Digite um número: "))
 print(f'O dobro de {n} vale {n * 2}.')
 print(f'O triplo de {n} vale {n * 3}.')
-print(f'A raiz quadrada de {n} vale {n ** (1/2):.2f}.')
+print(f'A raiz quadrada de {n} vale {n ** (1 / 2):.2f}.')
